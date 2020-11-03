ad9b10db926244e32b64c22aed9deae9f0764854
Jacin <jacinjiyan@gmail.com>
2020-11-03 16:10:57 +1100

    item class name

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -65,7 +65,7 @@ class TodoItemsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_item
-      @todo_item = todo_item.find(params[:id])
+      @todo_item = TodoItem.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
